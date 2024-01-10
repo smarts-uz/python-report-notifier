4814a123672b6f63b4677af18bee507d74bc549f
al-dossari <jumanazarov00@bk.ru>
2024-01-10 11:49:26 +0500

    saving to db

--- django-orm/main.py
+++ django-orm/main.py
@@ -19,24 +19,20 @@ django.setup()
 
 # Import your models for use in your script
 from db.models import *
-from db.saving_to_db import *
+
+
 ############################################################################
 ## START OF APPLICATION
 ############################################################################
 """ Replace the code below with your own """
 
 import datetime
+
 now = datetime.datetime.now()
 current_timestamp = datetime.datetime.now().astimezone().isoformat()
 
-
-
 # for u in Keyword.objects.all():
 #     print(f'ID: {u.id} \tUsername: {u.name} \tlast_checked: {u.last_checked}')
-
-
-
-
 
 
 # Test py functionality
@@ -48,18 +44,27 @@ p.users()
 p.chats()
 p.messages()
 
-def create_users(user_id,username,fullname):
+
+def create_users(user_id, username, fullname):
     Users.objects.create(
 
-        user_id = user_id,
-        username = username,
-        fullname = fullname
+        user_id=user_id,
+        username=username,
+        fullname=fullname
 
     )
-    print(f'Users has been saved!: user_id: {user_id} || username: {username} || {fullname}')
+    print(f'Users have been saved!: user_id: {user_id} || username: {username} || {fullname}')
 
-def create_chats():
-    pass
+
+def create_chats(chat_id, title, type):
+    Chats.objects.create(
+
+        chat_id=chat_id,
+        title=title,
+        type=type
+    )
+    print(f'Chats have been saved')
+
 
 def create_message(datetime, content, from_id, peer):
     Message.objects.create(
@@ -71,8 +76,24 @@ def create_message(datetime, content, from_id, peer):
 
     )
 
-    print('Message has been saved!! ')
+    print('Message have been saved!! ')
 
+
+with open('db/json/chats.json', mode='r', encoding='utf-8') as file:
+    data = json.load(file)
+    for chat in data:
+        chat_id = chat['chat_id']
+        title = chat['title']
+        type = chat['type']
+        create_chats(chat_id, title, type)
+
+with open('db/json/users.json', mode='r', encoding='utf-8') as file:
+    data = json.load(file)
+    for user in data:
+        user_id = user['user']
+        full_name = user['full_name']
+        username = user['username']
+        create_users(user_id,full_name,username)
 
 with open('db/json/messages.json', mode='r', encoding='utf-8') as file:
     data = json.load(file)
