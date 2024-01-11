e4b2166d935d8c68433f5af533b8e1a4266015c5
Samandar Komilov <komilov.samandar07@gmail.com>
2024-01-11 18:06:43 +0500

    Confusion

--- main.py
+++ main.py
@@ -18,10 +18,38 @@ import django
 django.setup()
 
 # Import your models for use in your script
-from db.models import *
+from django_orm.db.models import *
 
 ############################################################################
 ## START OF APPLICATION
 ############################################################################
 """ Replace the code below with your own """
 
+from datetime import datetime
+
+def db_check():
+    response
+    now = datetime.now()
+    current_timestamp = datetime.timestamp(now)
+    result = False
+    answer = input("enter key value:")
+    try:
+
+
+            natija = Keyword.objects.get(name=answer)
+            if natija is True:
+                return natija.lastcked
+
+
+    except:
+            Keyword.objects.create(
+                name = answer,
+                last_checked = now
+            )
+            Keyword.save()
+            return "True"
+
+
+
+
+    # for i in natija:
