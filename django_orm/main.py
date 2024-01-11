707dce50e025df053281d958b3025edf1d468be7
Samandar Komilov <komilov.samandar07@gmail.com>
2024-01-11 18:17:28 +0500

    Removed external main.py

--- django_orm/main.py
+++ django_orm/main.py
@@ -27,89 +27,32 @@ from db.models import *
 
 from datetime import datetime
 
-now = datetime.now()
-#
-# # Seed a few users in the database
-# Keyword.objects.create(
-#     name='#report1',
-#     last_checked = now
-# )
-# Keyword.objects.create(
-#     name='#LeftTheOffice2',
-#     last_checked = now
-# )
-# #
-# for u in Keyword.objects.all():
-#     print(f'ID: {u.id} \tUsername: {u.name}')
-#
-Users.objects.create(
-
-    fullname='huiaskjn',
-    username='supreme',
-    user_id=102153213,
-)
-user = Users.objects.all()
+# def db_check():
+#     response = ""
+#     now = datetime.now()
+#     current_timestamp = datetime.timestamp(now)
+#     result = False
+#     query = input("Enter query: ")
+#     try:
+#             response = Keyword.objects.filter(name__icontains=query)
+#             print(response)
+#             if response is True:
+#                 return natija.lastcked
 
 
-for i in user:
-
-    print(f'ID: {i.id} \tpeer_id: {i.fullname}')
+#     except:
+#             Keyword.objects.create(
+#                 name = answer,
+#                 last_checked = now
+#             )
+#             Keyword.save()
+#             return "True"
 
 
 
 
-# Message.objects.create(
-#
-#     name = 'rfiskwo',
-#     datetime ='2023-12-16',
-#     keyword_id = 1,
-#     content = 'text',
-#     from_id = 1235857,
-#     peer_id = 4324234
-# )
+#     # for i in natija:
 
-
-#
-# for u in Message.objects.all():
-#     print(f'ID: {u.id} \tpeer_id: {u.peer_id}')
-#
-# for u in Chats.objects.all():
-#     print(f'ID: {u.id} \ttitle: {u.title}')
-
-
-from Parsing.parser import Parser
-
-p = Parser()
-# users = p.users()
-# for user in users:
-#     user_id = user['user_id']
-#     fullname = user['fullname']
-#     username = user['username']
-#     Users.objects.create(
-#         user_id = user_id,
-#         fullname = fullname,
-#         username = username
-#     )
-#     print('success')
-
-#
-# Users.objects.create(
-#         user_id = 764782,
-#         fullname = 'thdj',
-#         username = 'hurihq'
-#     )
-
-#
-# chats = p.chats()
-# for chat in chats:
-#     chat_id = chat['chat_id']
-#     type = chat['type']
-#     title = chat['title']
-#     Chats.objects.create(
-#
-#         chat_id=chat_id,
-#         type=type,
-#         title=title
-#
-#     )
-#     print(f'success id:{chat_id}')
+query = input("Enter query: ")
+response = Keyword.objects.filter(name__icontains=query)
+print(response)
