b197ed87ce1eba0b2fc9187f1a2d5f16b3738312
al-dossari <jumanazarov00@bk.ru>
2024-01-22 12:24:41 +0500

    fixing cloud

--- django_orm/logx.py
+++ django_orm/logx.py
@@ -7,7 +7,7 @@ class Logger():
         self.logger = logging.getLogger(module_name)
         formater = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
         self.logger.info(f"Running module {module_name}...")
-        handler2 = logging.FileHandler(f"{logger_path()}/{module_name}.log", mode=fm, encoding='utf-8')
+        handler2 = logging.FileHandler(f"{logger_path()}{module_name}.log", mode=fm, encoding='utf-8')
         handler2.setFormatter(formater)
         self.logger.addHandler(handler2)
 
@@ -23,7 +23,7 @@ class Logger():
 
 def logger_path():
     year, month, day = datetime.now().year, datetime.now().month, datetime.now().day
-    logger_path = f"logs/{year}/{month}/{day}/"
+    logger_path = f"C:/Report-Logs/logs/{year}/{month}/{day}/"
     if not os.path.exists(logger_path):
         os.makedirs(logger_path)
     return logger_path
