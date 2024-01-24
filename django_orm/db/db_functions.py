415a3ceca2a81bec273fc6d979b663a971ea2bb5
Samandar-Komilov <komilov.samandar07@gmail.com>
2024-01-24 15:14:39 +0500

    Django ORM now works on any file

---
+++ django_orm/db/db_functions.py
@@ -0,0 +1,31 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+import django
+
+django.setup()
+
+from .models import *
+
+def all_keywords():
+    data = Keyword.objects.values('pk', 'name', 'last_checked', "topic_id")
+    return list(data)
+
+
+def forward_message_boolen(peer_id):
+    chat = Chat.objects.get(peer_id=peer_id)
+    return chat.forward_message
+
+
+def get_title_chat(peer_id):
+    chat = Chat.objects.get(peer_id=peer_id)
+    return chat.title
+
+
+def get_user_fullname(user_id):
+    user = User.objects.get(user_id=user_id)
+    return user.fullname, user.username
+
+
+def get_msg_id(msg_full_link):
+    message = Message.objects.get(message_full_link=msg_full_link)
+    return message.first.pk
--- django_orm/main.py
+++ django_orm/main.py
@@ -23,29 +23,9 @@ add_keyword_log = Logger('add_keyword',"a")
 
 #------- django orm function start
 
-def all_keywords():
-    data = Keyword.objects.values('pk', 'name', 'last_checked', "topic_id")
-    return list(data)
+from db.db_functions import *
 
 
-def forward_message_boolen(peer_id):
-    chat = Chat.objects.get(peer_id=peer_id)
-    return chat.forward_message
-
-
-def get_title_chat(peer_id):
-    chat = Chat.objects.get(peer_id=peer_id)
-    return chat.title
-
-
-def get_user_fullname(user_id):
-    user = User.objects.get(user_id=user_id)
-    return user.fullname, user.username
-
-
-def get_msg_id(msg_full_link):
-    message = Message.objects.get(message_full_link=msg_full_link)
-    return message.first.pk
 
 #django orm function end
 
