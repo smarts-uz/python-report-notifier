79d533383d03d16544da95b9e5b1a057f58d4a37
MuzaffarMamaroziqov <137500201+MuzaffarMamaroziqov@users.noreply.github.com>
2024-01-25 18:14:01 +0500

    Changes code to insert to navicat

---
+++ django_orm/getdialogs.py
@@ -0,0 +1,102 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+import django
+django.setup()
+
+from db.models import *
+
+import requests
+from datetime import datetime
+from pprint import pprint
+
+from logx import Logger
+save_channel_chat_log = Logger('save_channel_chat_log', 'a')
+
+url = "http://192.168.3.54:9503/api/messages.getDialogs"
+
+response = requests.post(url).json()
+def channel_dialogs():
+    data_channel = []
+    data_chat = []
+
+    for chat in response['response']['chats']:
+        if chat['_']=='chat' or chat['id']==2059626462:
+            continue
+        else:
+            # If channel
+            if chat['megagroup']==False:
+                 data_channel.append({
+                    "type": 'channel',
+                    "name": chat['title'],
+                    "tg_id":chat['id'],
+                    "signatures":chat['signatures'],
+                    "created_at" : datetime.fromtimestamp(chat['date']),
+                    })
+            else:
+                 data_chat.append({
+                    "type": 'chat',
+                    "name": chat['title'],
+                    "tg_id": chat['id'],
+                    "created_at": datetime.fromtimestamp(chat['date']),
+                    "participants_count": chat['participants_count'],
+                    })
+    return data_channel, data_chat
+
+
+# pprint(channel_dialogs())
+
+def saveChannels():
+    channels, chats = channel_dialogs()[0], channel_dialogs()[1]
+    for channel in channels:
+        try:
+            TgChannel.objects.get_or_create(**channel)
+            msg = f"{channel['name']} is saved!"
+            print(msg)
+            save_channel_chat_log.log(msg)
+        except Exception as e:
+            print("Error occured. Check the log file")
+            save_channel_chat_log.err(e)
+
+    for chat in chats:
+        try:
+            TgGroup.objects.get_or_create(**chat)
+            msg = f"{chat['name']} is saved!"
+            print(msg)
+            save_channel_chat_log.log(msg)
+        except Exception as e:
+            print("Error occured. Check the log file")
+            save_channel_chat_log.err(e)
+
+saveChannels()
+
+# for channel in data_channel:
+#     print(channel)
+#
+# for chat in data_chat:
+#     print(chat)
+
+# try:
+#     book = TgChannel.objects.get(name=i['title'])
+#
+# except TgChannel.DoesNotExist:
+#
+#     for channel_info in data_channel:
+#         TgChannel.objects.create(**channel_info)
+#
+# except TgChannel.MultipleObjectsReturned:
+#     pass
+#
+#
+#
+#
+# try:
+#     book = TgGroup.objects.get(name=i['title'])
+#
+# except TgGroup.DoesNotExist:
+#
+#     for channel_info in data_channel:
+#         TgGroup.objects.create(**channel_info)
+#
+# except TgGroup.MultipleObjectsReturned:
+#         pass
+#
