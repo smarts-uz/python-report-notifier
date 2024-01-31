f362c06193ce336ce19689a20538605346274ce0
Samandar-Komilov <komilov.samandar07@gmail.com>
2024-01-31 13:57:25 +0500

    Configured Logger, reassessment of TelegramRSS.py

--- logx.py
+++ logx.py
@@ -3,16 +3,22 @@ import os
 from datetime import datetime
 
 
-class Logger():
-    def __init__(self,module_name,fm):
+class Logger:
+    def __init__(self, module_name, fm, subdirectory=None):
         self.logger = logging.getLogger(module_name)
-        formater = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+        formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+
+        # Creating the log directory based on subdirectory parameter
+        log_directory = logger_path(subdirectory)
         self.logger.info(f"Running module {module_name}...")
-        handler2 = logging.FileHandler(f"{logger_path()}{module_name}.log", mode=fm, encoding='utf-8')
-        handler2.setFormatter(formater)
+
+        # Creating the FileHandler with the log file path
+        log_file_path = os.path.join(log_directory, f"{module_name}.log")
+        handler2 = logging.FileHandler(log_file_path, mode=fm, encoding='utf-8')
+        handler2.setFormatter(formatter)
         self.logger.addHandler(handler2)
 
-    def log(self,message):
+    def log(self, message):
         self.logger.setLevel(logging.DEBUG)
         self.logger.info(message)
 
@@ -21,10 +27,15 @@ class Logger():
         self.logger.exception(err)
 
 
-
-def logger_path():
+def logger_path(subdirectory = None):
     year, month, day = datetime.now().year, datetime.now().month, datetime.now().day
+
+    # Appending the subdirectory to the logger path if specified
     logger_path = f"C:/Report-Logs/logs/{year}/{month}/{day}/"
+    if subdirectory:
+        logger_path = os.path.join(logger_path, subdirectory)
+
+    # Creating the directory if it doesn't exist
     if not os.path.exists(logger_path):
         os.makedirs(logger_path)
     return logger_path
